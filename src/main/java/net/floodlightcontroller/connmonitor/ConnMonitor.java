2cb488088aaf859cd4c2aa547b2ed731584d25e3
xpan <xiangpan2011@u.northwestern.edu>
2015-01-30 21:22:31 -0600

    test3

--- src/main/java/net/floodlightcontroller/connmonitor/ConnMonitor.java
+++ src/main/java/net/floodlightcontroller/connmonitor/ConnMonitor.java
@@ -1416,33 +1416,21 @@ public class ConnMonitor extends ForwardingBase implements IFloodlightModule,IOF
             				ChecksumCalc.ETHERNET_HEADER_LEN,ChecksumCalc.ETHERNET_HEADER_LEN + ip_len);
             	byte[] new_tp_data = null;
             	if((new_src_port!=0) || (new_dst_port!=0)){
-            		System.err.println("  DEBUG Modify TP");
+            		
             		IPacket tp_pkt = ip_pkt.getPayload();
 	            	if(tp_pkt instanceof TCP){
-	            		System.err.println("    TCP original checksum:");
+	            		//System.err.println("    Modify TCP");
 	            		TCP tcp = (TCP)tp_pkt;
-	            		//short checksum = tcp.getChecksum();
-	    				//byte[] tcp_pkt_data = Arrays.copyOfRange(ip_pkt_data,ip_header_len,ip_len);
-	    				//System.err.println("    DATA1:"+bytesToHexString(tcp_pkt_data));
-	    				//System.err.println("    TCP original checksum:"+checksum+" "+tcp_pkt_data[16] +" "+tcp_pkt_data[17] );
 	    				if(new_src_port != 0)
 	    					tcp.setSourcePort(new_src_port);
 	    				if(new_dst_port != 0)
 	    					tcp.setDestinationPort(new_dst_port);
 	    				
 	    				tcp.resetChecksum();
-	    				//byte offset = tcp.getDataOffset();
-	    				//int len = offset<<2;
-	    				//checksum = tcp.getChecksum();
 	    				new_tp_data = tcp.serialize();
-	    				//System.err.println("    DATA2:"+bytesToHexString(new_data));
-	    				//checksum = ChecksumCalc.calculateTCPPacketChecksum(tcp_pkt_data,payload_len,src_ip,dst_ip);
-	    				//byte[] new_checksum_bytes = ByteBuffer.allocate(2).putShort(checksum).array();
-	    				//System.err.println("    TCP new checksum:"+checksum+" len:"+len+" "+payload_len+" "+new_data[16]+" "+new_data[17]);
-	    			
 	    			}
 	    			else if(tp_pkt instanceof UDP){
-	    				System.err.println("    UDP original checksum:");
+	    				//System.err.println("    UDP original checksum:");
 	    				UDP udp = (UDP)tp_pkt;
 	    				if(new_src_port != 0)
 	    					udp.setSourcePort(new_src_port);
@@ -1452,7 +1440,7 @@ public class ConnMonitor extends ForwardingBase implements IFloodlightModule,IOF
 	    				new_tp_data = udp.serialize();
 	    			}
 	    			else{
-	    				System.err.println("    Unknown IP packet. Ignore...");
+	    				System.err.println("    unknown IP packet. Ignore...");
 	    			}
 	            	
             	}
