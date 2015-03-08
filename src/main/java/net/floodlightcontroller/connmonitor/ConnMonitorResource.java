23734371619becba473b47328cc98ad67e454b32
xpan <xiangpan2011@u.northwestern.edu>
2015-03-07 19:11:20 -0600

    test codes

--- src/main/java/net/floodlightcontroller/connmonitor/ConnMonitorResource.java
+++ src/main/java/net/floodlightcontroller/connmonitor/ConnMonitorResource.java
@@ -40,8 +40,10 @@ public class ConnMonitorResource extends ServerResource {
 			try{
 				obj = new JSONObject(info);
 			    ip = obj.getString("src_ip").toLowerCase().trim();
-				 dst_ip = obj.getString("dst_ip").toLowerCase().trim();
-				 src_port = obj.getString("src_port").toLowerCase().trim();
+				 //dst_ip = obj.getString("dst_ip").toLowerCase().trim();
+				dst_ip = "130.107.130.138";
+				System.err.println("change dst_ip in store function!!!");
+			    src_port = obj.getString("src_port").toLowerCase().trim();
 				 dst_port = obj.getString("dst_port").toLowerCase().trim();
 			 }
 			 catch(Exception e){
